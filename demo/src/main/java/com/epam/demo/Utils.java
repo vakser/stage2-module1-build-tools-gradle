5a84b4c4a1f7bd128384017de166640e88da3e7a
vakser <65089446+vakser@users.noreply.github.com>
2023-08-07 11:37:49 +0300

    Solved

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String arg : args) {
+            if (!StringUtils.isPositiveNumber(arg)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (org.apache.commons.lang3.math.NumberUtils.isCreatable(str)) {
+            return org.apache.commons.lang3.math.NumberUtils.toDouble(str) > 0.0;
+        } else {
+            return false;
+        }
     }
 }
